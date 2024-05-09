0d7b5baa97061aae5a92ec07e7b7147cf5ea2da1
Vu Thanh Do (DMVN) <thanhdo9xi@gmail.com>
2024-05-09 09:12:52 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.js";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+dotenv.config();
+// khởi tạo
+const app = express();
+const server = http.createServer(app);
+app.use(express.json());
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", authRouter);
+mongoose.connect(process.env.URI);
+app.get("/", async (req, res) => {
+  res.json("success");
+});
+server.listen(process.env.PORT, (req, res) => {
+  try {
+    console.log(`Server is running on port ${process.env.PORT} `);
+  } catch (error) {
+    console.log(error);
+  }
+});
